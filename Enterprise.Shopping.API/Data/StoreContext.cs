5556f28e325f9632393eb9d3f06bf6ffd8832a38
LourenceQ <lawrenceqf@gmail.com>
2024-03-20 07:11:42 -0300

    implementando entidade para migrations

---
+++ Enterprise.Shopping.API/Data/StoreContext.cs
@@ -0,0 +1,13 @@
+﻿using Enterprise.Shopping.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Enterprise.Shopping.API.Data;
+
+public class StoreContext : DbContext
+{
+    public StoreContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+}
