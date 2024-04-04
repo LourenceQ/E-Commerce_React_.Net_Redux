7035f5b29579f5ee7963a510e9ed86258b451af8
LourenceQ <lawrenceqf@gmail.com>
2024-04-04 20:34:31 -0300

    iestilização de product card

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,8 +1,10 @@
 import {
+  Avatar,
   Button,
   Card,
   CardActions,
   CardContent,
+  CardHeader,
   CardMedia,
   Typography,
 } from "@mui/material";
@@ -14,17 +16,28 @@ interface Props {
 export default function ProductCard({ product }: Props) {
   return (
     <Card>
-      <CardMedia
-        image="http://picsum.photos/200"
-        title="title"
+      <CardHeader
+        avatar={<Avatar>{product.name?.charAt(0).toUpperCase()}</Avatar>}
+        title={product.name}
       />
+      <CardMedia
+        sx={{ height: 140 }}
+        image={product.pictureUrl}
+        title={product.name}
+      />
+      <CardMedia image="http://picsum.photos/200" title="title" />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
+        <Typography gutterBottom color='secondary' variant="h5">
+          {product.price}
+        </Typography>
+        <Typography  color='text.secondary' variant="body2">
+          {product.brand} / {product.type}
         </Typography>
       </CardContent>
+      
       <CardActions>
-        <Button size="small">Share</Button>
+        <Button size="small">Add to cart</Button>
+        <Button size="small">View</Button>
       </CardActions>
     </Card>
   );
