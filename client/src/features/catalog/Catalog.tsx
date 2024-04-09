2d0391d8f3dc87ed3f060aea4fbd6464e91d6f9b
LourenceQ <lawrenceqf@gmail.com>
2024-04-08 21:00:01 -0300

    refatoração de componentes

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,20 +1,21 @@
-import { Button } from "@mui/material";
 import { Product } from "../../app/models/products";
 import ProductList from "./ProductList";
+import { useEffect, useState } from "react";
 
-interface Props {
-  products: Product[];
-  addProduct: () => void;
-}
 
-export default function Catalog({ products, addProduct }: Props) {
+export default function Catalog() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch('https://localhost:7107/api/Products')
+    .then(response => response.json())
+    .then(data => setProducts(data))
+  }, [])
+   
   return (
     <>
       <h1>Catalog</h1>
       <ProductList products={products} />
-      <Button variant="contained" onClick={addProduct}>
-        Add product
-      </Button>
     </>
   );
 }
