b81b3bcd9865d68b92e5fbe7c9d228839f9870e6
LourenceQ <lawrenceqf@gmail.com>
2024-04-09 21:23:59 -0300

    implementando dark mode

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,17 +1,25 @@
 import Catalog from "../../features/catalog/Catalog";
-import { Container, CssBaseline } from "@mui/material";
+import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import Header from "../../features/catalog/Header";
+import { useState } from "react";
 
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'light' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: paletteType
+    }
+  })
   return (
-    <>
+    <ThemeProvider theme={theme}>
     <CssBaseline />
       <Header/>
       <Container>
         <Catalog/>
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
