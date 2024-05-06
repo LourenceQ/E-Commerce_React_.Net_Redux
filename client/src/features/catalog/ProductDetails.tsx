114b58cb43e25f8f0741dc6bfeb04dd888badf68
LourenceQ <lawrenceqf@gmail.com>
2024-05-06 20:54:30 -0300

    correção url de endpoint de get product

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -11,11 +11,15 @@ export default function ProductDetails() {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:5000/api/products/${id}`)
+      .get(`https://localhost:7107/api/products/${id}`)
       .then((response) => setProduct(response.data))
       .catch((error) => console.log(error))
       .finally(() => setLoading(false));
   }, [id]);
 
-  return <Typography variant="h2">Product Details</Typography>;
+  if (loading) return <h3>Loading...</h3>;
+
+  if (!product) return <h3>Product not found.</h3>;
+
+  return <Typography variant="h2">{product.name}</Typography>;
 }
